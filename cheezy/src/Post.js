021cf533ba49f1fbda72f875997b2db87f519cc9
Matthew Binshtok <matthew.binshtok@gmail.com>
2018-06-06 15:13:01 -0400

    Updated version of post

--- cheezy/src/Post.js
+++ cheezy/src/Post.js
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import './Post.css';
 
 class Post extends Component {
+
+     constructor(){
+          super();
+          this.state = {
+               author: "author",
+               caption: "I went to the beach",
+               imageUrl: "",
+               profileUrl: "",
+               likes: 0,
+               cheese: 0,
+               date: "",
+               comments: []
+          }
+     }
+
      render() {
+
+
+
           return (
                <div className="Post">
-               <header className="Post-header">
-                    <p className="post-author-name"> Author </p>
-               </header>
+                    <div className="Post-header">
+                         <p className="author-name"> {this.state.author} </p>
+                         <img className="profile-picture" src={this.state.profileUrl} />
+                    </div>
+                    <div className="picture-container">
+                         <img src={this.state.imageUrl} />
+                    </div>
+                    <div className="captions-actions-container">
+                         <div className="caption-container">
+                              <p className="caption-label"> {this.state.caption} </p>
+                         </div>
+                         <div className="actions-container">
+                              <button className="like-button"></button>
+                              <button className="cheese-button"></button>
+                         </div>
+                    </div>
 
                </div>
           );
