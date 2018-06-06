b468d8569f10d5a7454a7ce2049ecea996f87fb2
Matthew Binshtok <matthew.binshtok@gmail.com>
2018-06-06 14:38:41 -0400

    Added post files and raw structure

---
+++ cheezy/src/Post.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Post.css';
+
+class Post extends Component {
+     render() {
+          return (
+               <div className="Post">
+               <header className="Post-header">
+                    <p className="post-author-name"> Author </p>
+               </header>
+
+               </div>
+          );
+     }
+}
+
+export default Post;
